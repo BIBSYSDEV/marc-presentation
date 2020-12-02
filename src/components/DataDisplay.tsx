0e9560a5cbd997c0a952150b74a618ac78602672
Gregor Gabriel <gregor.gabriel@unit.no>
2020-12-02 14:51:29 +0100

    SMILE-329: finpusse på linjepresentasjon

--- src/components/DataDisplay.tsx
+++ src/components/DataDisplay.tsx
@@ -31,104 +31,94 @@ const DataDisplay: FC = (props: any) => {
       const xmlDoc = parser.parseFromString(xmlData, "text/xml");
       let rootNode = xmlDoc.getElementsByTagName("record").item(0);
 
+      function extractSubfields(parentNode: ChildNode, j: number) {
+        let returnString: string = "";
+        for (let i = 0; i < parentNode.childNodes[j].childNodes.length; i++) {
+          if (parentNode.childNodes[j].childNodes[i]) {
+            let theNode = parentNode.childNodes[j].childNodes[i].parentElement;
+            if (theNode) {
+              if (theNode.getAttribute("code") != null) {
+                let subcode = theNode.getAttribute("code")!.toString().trim();
+                let value = parentNode.childNodes[j].childNodes[i].textContent!.trim();
+                returnString += ` $${subcode}${value}`.trimEnd();
+              }
+            }
+          }
+        }
+        return returnString;
+      }
+
+      function extractMarcDatafields(parentNode: ChildNode) {
+        let returnString: string = "";
+        for (let j = 1; j < parentNode.childNodes.length; j++) {
+          if (parentNode.nodeName !== "controlfield") {
+            let theNode = parentNode.childNodes[j].parentElement;
+            if (theNode && j === 1) {
+              let ind1: string = "";
+              let ind2: string = "";
+              if (theNode.getAttribute("ind1") != null) {
+                if (theNode.getAttribute("ind1")!.toString() === " ") {
+                  ind1 = "#";
+                } else {
+                  ind1 = theNode.getAttribute("ind1")!.toString().trim();
+                }
+              }
+              if (theNode.getAttribute("ind2") != null) {
+                if (theNode.getAttribute("ind2")!.toString() === " ") {
+                  ind2 = "#";
+                } else {
+                  ind2 = theNode.getAttribute("ind2")!.toString().trim();
+                }
+              }
+              if (theNode.getAttribute("tag") != null) {
+                let marccode = theNode.getAttribute("tag")!.toString().trim();
+                returnString += `*${marccode} ${ind1} ${ind2}`;
+              }
+            }
+            returnString += extractSubfields(parentNode, j);
+          }
+        }
+        if (returnString.trim() !== "") {
+          return returnString + "\n";
+        } else {
+          return returnString;
+        }
+      }
+
+      function extractMarcControlfields(parentNode: ChildNode, i: number) {
+        let returnString: string = "";
+        if (parentNode.childNodes[i]) {
+          let theNode = parentNode.childNodes[i].parentElement;
+          if (theNode) {
+            let value: string = parentNode.childNodes[i].nodeValue!.trim();
+            if (theNode.getAttribute("tag") != null) {
+              let marccode: string = theNode.getAttribute("tag")!.toString().trim();
+              returnString = `*${marccode} ${value}`.trim();
+            } else if (theNode.tagName === "leader") {
+              returnString = `*ldr ${value}`.trim();
+            }
+          }
+        }
+        return returnString;
+      }
+
       const findChildNodeValue = (parentNode: ChildNode | null): string => {
         if (parentNode === null) {
           return "Missing rootnode";
         }
-        let returnString: string = "";
-        let tempString: string = "";
 
         for (let i = 0; i < parentNode.childNodes.length; i++) {
-          if (parentNode.childNodes[i]) {
-            let theNode = parentNode.childNodes[i].parentElement;
-            if (theNode) {
-              if (theNode.getAttribute("tag") != null) {
-                tempString +=
-                    "*" +
-                    theNode
-                    .getAttribute("tag")!
-                    .toString()
-                    .trim() +
-                    " ";
-              } else if (theNode.tagName === "leader") {
-                tempString += "*ldr ";
-              }
-            }
-            returnString += tempString;
-            tempString = "";
-            tempString += parentNode.childNodes[i].nodeValue;
-            returnString += tempString.trim() + " ";
-            tempString = "";
-          }
           if (parentNode.nodeName !== "datafield") {
-            return returnString;
+            return extractMarcControlfields(parentNode, i) + "\n";
           }
         }
 
-        returnString = "";
-
-        for (let j = 1; j < parentNode.childNodes.length; j++) {
-          let theNode = parentNode.childNodes[j].parentElement;
-          if (theNode && j === 1) {
-            let ind1 = "";
-            let ind2 = "";
-            if (theNode.getAttribute("ind1") != null) {
-              if (theNode.getAttribute("ind1")!.toString() === " ") {
-                ind1 = "#";
-              } else {
-                ind1 = theNode.getAttribute("ind1")!.toString();
-              }
-            }
-            if (theNode.getAttribute("ind2") != null) {
-              if (theNode.getAttribute("ind2")!.toString() === " ") {
-                ind2 = "#";
-              } else {
-                ind2 = theNode.getAttribute("ind2")!.toString();
-              }
-            }
-            if (theNode.getAttribute("tag") != null) {
-              tempString +=
-                  "*" +
-                  theNode
-                  .getAttribute("tag")!
-                  .toString()
-                  .trim() +
-                  " " +
-                  ind1 +
-                  " " +
-                  ind2 +
-                  " ";
-            }
-          }
-          for (let i = 0; i < parentNode.childNodes[j].childNodes.length; i++) {
-            if (parentNode.childNodes[j].childNodes[i]) {
-              let theNode = parentNode.childNodes[j].childNodes[i].parentElement;
-              if (theNode) {
-                if (theNode.getAttribute("code") != null) {
-                  tempString +=
-                      "$" +
-                      theNode
-                      .getAttribute("code")!
-                      .toString()
-                      .trim();
-                }
-              }
-              returnString += tempString;
-              tempString = "";
-              tempString += parentNode.childNodes[j].childNodes[i].textContent;
-              returnString += tempString.trim() + " ";
-              tempString = "";
-            }
-          }
-        }
-
-        returnString += "\n";
-        return returnString;
+        return extractMarcDatafields(parentNode);
       };
 
       const showData = (): string => {
         let returnString: string = "";
-        if (showAsXML === true) {
+        if (showAsXML) {
           return xmlData;
         } else {
           if (rootNode) {

4ba1b350e703cba7090cf434fd44888175d30102
Gregor Gabriel <gregor.gabriel@unit.no>
2020-12-16 15:10:56 +0100

    changed url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from "@material-ui/core/Button";
 
 //const almaSruUrl = 'https://bibsys.alma.exlibrisgroup.com/view/sru/47BIBSYS_NETWORK?operation=searchRetrieve&version=1.2&query=alma.mms_id="991325803064702201"';
 const almaSruUrl = 'https://6kdv0kt7h3.execute-api.eu-west-1.amazonaws.com/dev/alma?mms_id="991325803064702201"';
-const authoritySruUrl = 'https://mhddpy4awa.execute-api.eu-west-1.amazonaws.com/v1/authority/?auth_id=1093967';
+const authoritySruUrl = 'https://adimyg90ik.execute-api.eu-west-1.amazonaws.com/v1/?auth_id=1093967';
 
 function App() {
   const [showXMLPressed, setShowXMLPressed] = useState(true);

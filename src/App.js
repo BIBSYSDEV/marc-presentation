47448e3872438ac4c42047820f1163688cf22fe7
Gregor Gabriel <gregor.gabriel@unit.no>
2020-12-18 10:16:48 +0100

    use alias url

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import DataDisplay from "./components/DataDisplay.tsx";
 import Button from "@material-ui/core/Button";
 
 //const almaSruUrl = 'https://bibsys.alma.exlibrisgroup.com/view/sru/47BIBSYS_NETWORK?operation=searchRetrieve&version=1.2&query=alma.mms_id="991325803064702201"';
-const almaSruUrl = 'https://6kdv0kt7h3.execute-api.eu-west-1.amazonaws.com/dev/alma?mms_id="991325803064702201"';
-const authoritySruUrl = 'https://exqjrbue2a.execute-api.eu-west-1.amazonaws.com/v1/authority?auth_id=1093967';
+const almaSruUrl = 'https://api.sandbox.bibs.aws.unit.no/marc?mms_id="991325803064702201"';
+const authoritySruUrl = 'https://api.sandbox.bibs.aws.unit.no/authority?auth_id=1093967';
 
 function App() {
   const [showXMLPressed, setShowXMLPressed] = useState(true);

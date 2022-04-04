bf1a3782f9cd01598c2b801be9ce5d270f54252e
pcb <pcb@unit.no>
2022-04-04 09:34:56 +0200

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { USE_MOCK_DATA } from './constants';
 import { interceptRequestsOnMock } from './mock-interceptors';
+import { createRoot } from 'react-dom/client';
 
-if (USE_MOCK_DATA) {
-  interceptRequestsOnMock();
+const container = document.getElementById('root');
+if (container) {
+  if (USE_MOCK_DATA) {
+    interceptRequestsOnMock();
+  }
+  createRoot(container).render(<App />);
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
